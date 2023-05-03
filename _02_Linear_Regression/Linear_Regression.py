398d3f47354449086f2c0a32a6c6dbe632b9aaae
= <=>
2023-05-03 22:55:17 +0800

    '2'

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,9 +9,25 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
+    x,y=read_data()
+    lam=2e-10
+    weight=np.matmul(np.linalg.pinv(np.matmul(x.T,x)+np.matmul(,np.eye(6))),np.matmul(x.T,y))
+    return weight @ data
     pass
     
 def lasso(data):
+    x, y = read_data()
+    weight = np.ones(6)
+    rate = 1e-12
+    label = 1e-5
+    for i in range(int(1e10)):
+        Y = np.matmul(weight, x)
+        loss = np.sum(Y - y) ** 2 + np.linalg.norm(weight,ord=1)
+        if loss < label
+            break
+        dweight=np.matmul((Y - y),x.T)
+        weight=weight-rate * dweight
+    return weight @ data
     pass
 
 def read_data(path='./data/exp02/'):
