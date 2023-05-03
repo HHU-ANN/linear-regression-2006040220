b0d4b216bfd3367422147ddd7ea36998d37da06c
= <=>
2023-05-04 00:22:50 +0800

    '2'

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -22,7 +22,7 @@ def lasso(data):
     label = 1e-7
     for i in range(int(1e5)):
         Y = np.matmul(weight, x.T)
-        loss = (np.sum(Y - y) ** 2 + 10*np.linalg.norm(weight,ord=1))/6
+        loss = (np.sum(Y - y) ** 2 + 0.001*np.linalg.norm(weight,ord=1))/6
         if loss < label :
             break
         dweight=np.matmul((Y - y).T,x)
